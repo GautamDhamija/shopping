9f6026187f0faeed7a8f78bac0534176531e0d6b
GautamDhamija <45586761+GautamDhamija@users.noreply.github.com>
2020-03-17 07:18:04 +0530

    Add files via upload

---
+++ prices.js
@@ -0,0 +1,18 @@
+function pp1(){//price of product 1
+	return 50;
+}
+function pp2(){//price of product 1
+	return 50;
+}
+function pp3(){//price of product 1
+	return 100;
+}
+function pp4(){//price of product 1
+	return 100;
+}
+function pp5(){//price of product 1
+	return 150;
+}
+function pp6(){//price of product 1
+	return 150;
+}
